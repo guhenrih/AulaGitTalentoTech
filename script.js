70bebd61bbc25ef447ed238d1318cc5af0d80283
Gustavo Henrique de Almeida <gualmeida060@gmail.com>
2024-12-05 21:45:40 -0300

    codigo quase pronto

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 fetch('data.txt')
-    .then(response => response.text())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erro ao carregar o arquivo.");
+        }
+        return response.text();
+    })
     .then(data => {
         document.getElementById('dynamic-text').textContent = data;
     })
     .catch(error => {
         document.getElementById('dynamic-text').textContent = 'Erro ao carregar texto dinâmico.';
+        console.error(error);
     });
